ae9f6a19a785ea06820c61b327af963d5dc481db
Михаил <40540053+egorovmikhail@users.noreply.github.com>
2020-10-22 06:11:43 +1100

    Add files via upload

---
+++ KIZ_UIKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  KIZ_UIKit
+//
+//  Created by Михаил Егоров on 22.10.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    view.backgroundColor = .systemPink
+  }
+
+
+}
+
